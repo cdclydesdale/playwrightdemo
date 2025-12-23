217cf35888f0a3970979f110a47c8cc1e76b68ea
Clyde Dsouza <cdclydesdale@gmail.com>
2025-12-23 12:46:01 -0500

    Implemented Browser Context in Playwright

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,14 +1,27 @@
-import {test, expect, Browser, Page} from '@playwright/test';
+import {test, expect, Browser, Page, BrowserContext} from '@playwright/test';
 import {chrommium, firefox, webkit} from 'playwright';
 
 test('Login Test', async() => {
 const browser:Browser = await webkit.launch({ headless: true });
-const page:Page = await browser.newPage();
-await page.goto('https://naveenautomationlabs.com/opencart/index.php?route=account/login');
-await page.fill('input#input-email', 'pwtest@opencart.com');
-await page.fill('input#input-password', 'playwright@123');
-await page.click('input[value="Login"]');
-await expect(page).toHaveTitle('Account Login');
+
+const browserContext1 = await browser.newContext();
+
+
+const page1:Page = await browserContext1.newPage();
+await page1.goto('https://naveenautomationlabs.com/opencart/index.php?route=account/login');
+await page1.fill('input#input-email', 'pwtest@opencart.com');
+await page1.fill('input#input-password', 'playwright@123');
+await page1.click('input[value="Login"]');
+await expect(page1).toHaveTitle('My Account');
+await browserContext1.close();
+
+//browserContext2
+const browserContext2 = await browser.newContext();
+const page2:Page = await browserContext2.newPage();
+await page2.goto('https://naveenautomationlabs.com/opencart/index.php?route=account/login');
+await expect(page2).toHaveTitle("Account Login");
+await browserContext2.close(); 
+
 await browser.close();
 
 });
